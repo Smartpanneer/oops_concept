1a4c3bdff89cdee8c7ee5d861f4eddbfe0de9430
panneer <panneer.ece2020@gmail.com>
2025-11-12 21:10:35 +0530

    inter

--- stu_class.py
+++ stu_class.py
@@ -1,7 +1,12 @@
-class Student():
-    def exam(self,mark):
-        return f'The student scored in the exam {mark}/500'
+class College:
+   def result(self,m,c,p):
+        maths=m
+        chemistry=c/2
+        physics=p/2
+        mark=maths+chemistry+physics
+        return f'The student 12th cut off mark is  {mark}'
 
-obj=Student()
-m=430
-print(obj.exam(m))
+stu_1=College()
+stu_2=College()
+print(stu_1.result(100,100,100))
+print(stu_2.result(78,62,79))
